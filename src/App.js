1b5c9edeb80f58dfcd9bf6e40bbdbb9f920c50be
anmolkohli <anmol.kohli18@gmail.com>
2021-01-25 09:02:14 +0530

    new login

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import MyRoutes from './MyRoutes';
-
-const theme = createMuiTheme({
-  palette: {
-    secondary: {
-      main: '#f48fb1',
-    },
-  },
-});
 
 function App() {
   return (
     <div className="App">
-      <ThemeProvider theme={theme}>
-        <MyRoutes />
-      </ThemeProvider>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
